e57bd0f2716c3e44e30c08ddf10e47d848783bf2
“Marlon <marlonperalta2009@gmail.com>
2023-08-29 16:30:22 -0600

    Ejemplo de un generador de contraseñas

---
+++ generator/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+# Create your views here.
+def home(request):
+    return render(request, 'generator/home.html')
+
+def about(request):
+    return render(request, 'generator/about.html')
+
+def password(request):
+    
+    characters = list('abcdefghijklmnñopqrstuvwxyz')
+    generator_password=''
+    
+    length = int(request.GET.get('length'))
+    
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'))
+        
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%ˆ&*()'))
+        
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+    
+    for x in range(length):
+        generator_password += random.choice(characters)
+        
+    
+    return render(request, 'generator/password.html', {'password': generator_password})
+    
+
+
+
